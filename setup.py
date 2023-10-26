c4db1950fe2a1915d5e5440f11fcc2ac00e6a15d
caibingcheng <jack_cbc@163.com>
2023-10-26 19:37:24 +0800

    [fix] publish version must be PEP 440 compliant

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ import os
 import time
 
 current_timestamp = time.time()
-version = os.environ.get('VERSION', 'UNKNOWN-{}'.format(current_timestamp))
+version = os.environ.get('VERSION', '')
+if version == '':
+    raise Exception('VERSION environment variable not set')
+
+# the original version is refs/tags/V0.6.2
+# version must be PEP 440 compliant
+version = version.replace('refs/tags/V', '')
 print('Version: {}'.format(version))
 
 setup(
     name='fkfish',
     version=version,
-    author="bbing",
+    author="caibingcheng",
+    author_email="jack_cbc@163.com",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
